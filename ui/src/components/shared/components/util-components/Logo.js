cd93a595b926108b3a11126bb1b97e8168e01dd7
VidyasagarPandikasahala <vidya.sagar.six@gmail.com>
2023-08-29 15:29:07 +0530

    [UI] [HomePage]  custom logo for the app

---
+++ ui/src/components/shared/components/util-components/Logo.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Logo.module.css";
+const Logo = () => {
+  return (
+    <>
+      <div className={styles.logoContainer}>
+        <h1 className={styles.logo}> MP </h1>
+      </div>
+    </>
+  );
+};
+
+export default Logo;
