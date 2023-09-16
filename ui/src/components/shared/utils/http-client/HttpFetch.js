193931ba46e731a4a300a231243846f271315fa6
VidyasagarPandikasahala <vidya.sagar.six@gmail.com>
2023-09-16 15:35:43 +0530

    Made the HTTP fetch more reusable

--- ui/src/components/shared/utils/http-client/HttpFetch.js
+++ ui/src/components/shared/utils/http-client/HttpFetch.js
@@ -21,9 +21,7 @@ export default class HttpFetch {
       // cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
       // credentials: "same-origin", // include, *same-origin, omit
       headers: {
-        "Content-Type": "application/json",
         ...params,
-        // 'Content-Type': 'application/x-www-form-urlencoded',
       },
       redirect: "follow", // manual, *follow, error
       referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
