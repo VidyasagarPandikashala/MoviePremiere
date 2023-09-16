7696cb3874c5bfdefbd7ff91c6ac4604ada37d73
VidyasagarPandikasahala <vidya.sagar.six@gmail.com>
2023-09-16 15:35:21 +0530

    Login API the headers are passed as parameters

--- ui/src/components/pages/login/api/loginApi.js
+++ ui/src/components/pages/login/api/loginApi.js
@@ -8,18 +8,22 @@ const loginApi = {
 
 function postUserData(data) {
   console.log(data);
-  const url = `http://localhost:8083/movie-premiere/v1/sign-up`;
+  const url = `http://localhost:8083/movie-premiere/v1/user/sign-up`;
   return HttpFetch.post(url, {}, data);
 }
 function authenticateUserData(data) {
-  const url = `http://localhost:8083/movie-premiere/v1/`;
-  return HttpFetch.post(url, {}, data);
+  const url = `http://localhost:8083/movie-premiere/v1/user`;
+  const headers = {
+    "Content-Type": "application/json",
+    Accept: "application/json", // Add this line
+  };
+  return HttpFetch.post(url, headers, data);
 }
 function logout(userId) {
   const queryParams = new URLSearchParams();
   queryParams.append("id", userId);
 
-  const url = `http://localhost:8083/movie-premiere/v1/logout?${queryParams.toString()}`;
+  const url = `http://localhost:8083/movie-premiere/v1/user/logout?${queryParams.toString()}`;
   return HttpFetch.post(url, {});
 }
 export default loginApi;
