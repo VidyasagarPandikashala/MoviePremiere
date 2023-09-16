e1d9b2184aa66840207eeef33b835439e12b95b0
VidyasagarPandikasahala <vidya.sagar.six@gmail.com>
2023-09-16 15:29:26 +0530

    Removed unwanted dependencies on the comp

--- ui/src/components/pages/signup/api/signupApi.js
+++ ui/src/components/pages/signup/api/signupApi.js
@@ -1,25 +1,16 @@
-import HttpFetch from "../../../shared/utils/http-client/HttpFetch.js";
+import HttpFetch from "../../../shared/utils/http-client/HttpFetch";
 
 const signupApi = {
   postUserData: postUserData,
-  authenticate: authenticateUserData,
-  logout: logout,
 };
 
 function postUserData(data) {
-  console.log(data);
-  const url = `http://localhost:8083/movie-premiere/v1/sign-up`;
-  return HttpFetch.post(url, {}, data);
+  const url = `http://localhost:8083/movie-premiere/v1/user/sign-up`;
+  const headers = {
+    "Content-Type": "application/json",
+    Accept: "application/json", // Add this line
+  };
+  return HttpFetch.post(url, headers, data);
 }
-function authenticateUserData(data) {
-  const url = `http://localhost:8083/movie-premiere/v1/`;
-  return HttpFetch.post(url, {}, data);
-}
-function logout(userId) {
-  const queryParams = new URLSearchParams();
-  queryParams.append("id", userId);
 
-  const url = `http://localhost:8083/movie-premiere/v1/logout?${queryParams.toString()}`;
-  return HttpFetch.post(url, {});
-}
 export default signupApi;
