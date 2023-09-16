460dd9aaed809719adf841319fddcd930c01d619
VidyasagarPandikasahala <vidya.sagar.six@gmail.com>
2023-09-16 15:25:54 +0530

    Refactored Fav comp - Added movie Card Added action buttons to redirect to login and sign up Added Redirection to respective pages

--- ui/src/components/pages/favourite/page/FavouritePage.js
+++ ui/src/components/pages/favourite/page/FavouritePage.js
@@ -1,50 +1,67 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import FavouritesIcon from "../../../shared/components/util-components/FavouritesIcon";
-import { getAllfavouritesForCurrentUser } from "../redux/FavouritesSlice";
+import { clearData } from "../redux/FavouritesSlice";
 import { FavouritePageApi } from "../api/favouritesPageApi";
+import MovieCard from "../../../shared/components/util-components/MovieCard";
+import styles from "./FavouritePage.module.css";
+import { Link } from "react-router-dom";
+import Logo from "../../../shared/components/util-components/Logo";
+import Button from "../../../shared/components/util-components/Button";
 
 function FavouritePage() {
   const selector = useSelector((state) => state.favourite.favouriteMovieIds);
+  const loginStatus = useSelector((state) => state.login.isLoggedIn);
+  const dispatch = useDispatch();
   console.log(selector);
   const [favMovies, setFavMovies] = useState([]);
-  const dispatch = useDispatch();
 
   useEffect(() => {
-    setFavMovies([]);
-    selector.forEach((eachId) => {
-      FavouritePageApi.getMovieDataForFavourites(eachId, 1).then((data) => {
-        setFavMovies((prevFavMovies) => [...prevFavMovies, data]);
+    if (loginStatus) {
+      setFavMovies([]);
+      selector.forEach((eachId) => {
+        FavouritePageApi.getMovieDataForFavourites(eachId, 1).then((data) => {
+          setFavMovies((prevFavMovies) => [...prevFavMovies, data]);
+        });
       });
-    });
-  }, [selector]);
+    } else {
+      setFavMovies([]);
+      dispatch(clearData());
+    }
+  }, [selector, loginStatus, dispatch]);
+
+  if (!loginStatus) {
+    return (
+      <>
+        <h3>If not loggedIn Please click the button below</h3>
+        <Button action={"login"} redirect={"login"}></Button>
+        <Button action={"sign-up"} redirect={"sign-up"}></Button>
+      </>
+    );
+  }
 
   if (favMovies.length === 0) {
     return <div>No favourites...</div>;
   }
   return (
-    <div>
-      {favMovies.map((eachMovie) => {
-        return (
-          <div>
-            <p>{eachMovie.movieName}</p>
-            <FavouritesIcon
-              onToggle={function handleFavouritesToggle() {
-                FavouritePageApi.toggleActionInFavourites(eachMovie.movieId)
-                  .then((response) => response.json())
-                  .then((data) => {
-                    dispatch(getAllfavouritesForCurrentUser());
-                  });
-              }}
-              classStyle={
-                selector.has(eachMovie.movieId) ? "iconAdded" : "iconNotAdded"
-              }
-            ></FavouritesIcon>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <Link to="/">
+        <Logo></Logo>
+      </Link>
+      <div className={styles.movieCard}>
+        {favMovies.map((movieData) => {
+          return (
+            <MovieCard
+              key={movieData.movieId}
+              customClassNameFavIcon={"favIconContainerFavPage"}
+              movieData={movieData}
+              favouriteExist={selector.has(movieData.movieId)}
+              customContainerClassName={"sectionFavCardContainer"}
+            ></MovieCard>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
