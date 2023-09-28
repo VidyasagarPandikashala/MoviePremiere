220ab2cfa0ed3e6daad14597badd0999dfbfa990
VidyasagarPandikasahala <vidya.sagar.six@gmail.com>
2023-09-28 18:35:39 +0530

    movieCard.js:changed html and css : responsive
    text displayed is uppercase(maintained throughout application)
    responsiveness in progress
    redesgined the MovieCard to have cleaner look

--- ui/src/components/shared/components/util-components/MovieCard.js
+++ ui/src/components/shared/components/util-components/MovieCard.js
@@ -20,31 +20,36 @@ function MovieCard({
       <div className={styles[customContainerClassName]}>
         <Link to={`/movie/${movieData.movieId}`} rel="">
           <img
+            loading="lazy"
             src={movieData.imageUrl}
             className={styles.image}
             alt={movieData.movieName}
           ></img>
           <div className="imageOverlay"></div>
           <div className={styles.movieDetailOverlay}>
-            <h2 className={styles.movieName}>{movieData.movieName}</h2>
+            <h2 className={styles.movieName}>
+              {movieData.movieName.toUpperCase()}
+            </h2>
             <p className={styles.releaseDate}>{movieData.releaseDate}</p>
           </div>
         </Link>
-        <FavouritesIcon
-          customClassNameFavIcon={customClassNameFavIcon}
-          onToggle={function handleFavouritesToggle() {
-            if (loginStatus) {
-              FavouritePageApi.toggleActionInFavourites(movieData.movieId)
-                .then((response) => response.json())
-                .then((data) => {
-                  dispatch(getAllfavouritesForCurrentUser());
-                });
-            } else {
-              navigate("/login");
-            }
-          }}
-          classStyle={favouriteExist ? "iconAdded" : "iconNotAdded"}
-        />
+        <div className={styles.favourites_icon}>
+          <FavouritesIcon
+            customClassNameFavIcon={customClassNameFavIcon}
+            onToggle={function handleFavouritesToggle() {
+              if (loginStatus) {
+                FavouritePageApi.toggleActionInFavourites(movieData.movieId)
+                  .then((response) => response.json())
+                  .then((data) => {
+                    dispatch(getAllfavouritesForCurrentUser());
+                  });
+              } else {
+                navigate("/login");
+              }
+            }}
+            classStyle={favouriteExist ? "iconAdded" : "iconNotAdded"}
+          />
+        </div>
       </div>
     </>
   );
